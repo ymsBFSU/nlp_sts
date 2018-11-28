842ac4ae1c3cb5fc86c84871d24be476969c5e2a
jforjohn <giannisotiro@gmail.com>
2018-11-28 18:22:06 +0100

    LAUNCHER: a run all tests for all models

--- MyLauncher.py
+++ MyLauncher.py
@@ -1,12 +1,15 @@
 ##
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from scipy.stats import pearsonr
 from MyModel import STSModels
+import numpy as np
+from sklearn.svm import SVR
+from sklearn.ensemble import AdaBoostRegressor
 
 from nltk import download
 from nltk.corpus import treebank
 from nltk.tag.perceptron import PerceptronTagger
-download('treebank')
+#download('treebank')
 
 def ptTrain():
     train_data = treebank.tagged_sents()
@@ -15,24 +18,108 @@ def ptTrain():
     per.train(train_data)
     return per
 
-filename = 'SMTeuroparl'
+filenames_train = ['MSRpar', 'MSRvid', 'SMTeuroparl']
+filenames_test = ['MSRpar', 'MSRvid', 'SMTeuroparl',
+				  'surprise.OnWN', 'surprise.SMTnews']
 ##
-print('Train PosTagger')
-per = ptTrain()
-##
-print('Train of Lemmas Model')
-trn = STSModels()
-trn.fit_lemmas(filename, 'train', per)
-##
-regr = LinearRegression().fit(trn.X, trn.y)
-##
-print('Test of Lemmas Model')
-tst = STSModels()
-tst.fit_lemmas(filename, 'test', per)
-##
-print('Prediction')
-pred = regr.predict(tst.X)
+#print('Train PosTagger')
+per = ''#ptTrain()
 
-result = pearsonr(pred, tst.y)[0] # taking just the Pearson coeficient
-print(result)
-#print(tst.y - pred)
+for test_file in filenames_test:
+	print(test_file)
+	filename_test = [test_file]
+	## 
+	#############################################################
+	print()
+	print('#####################################')
+	print()
+	# Lemmas
+
+	print('Train of Lemmas Model')
+	trn = STSModels()
+	trn.fit(filenames_train, ['lemmas'], 'train', per)
+	##
+	X_lem = trn.X
+	regr = AdaBoostRegressor().fit(trn.X, trn.y)
+	#regr = LogisticRegression(solver='lbfgs',
+	#                          multi_class='auto').fit(trn.X, trn.y)
+	##
+	print('Test of Lemmas Model')
+	tst = STSModels()
+	tst.fit(filename_test, ['lemmas'], 'test', per)
+	Xtst_lemma = tst.X
+	##
+	print('Prediction')
+	pred = regr.predict(tst.X)
+
+	result = pearsonr(pred, tst.y)[0] # taking just the Pearson coeficient
+	print(result)
+
+	## 
+	#############################################################
+	print()
+	print('#####################################')
+	print()
+	# Lesk
+	print('Train of Lesk Model')
+	trn = STSModels()
+	trn.fit(filenames_train, ['lesk'], 'train', per)
+	X_lesk = trn.X
+	##
+	regr = AdaBoostRegressor().fit(trn.X, trn.y)
+	##
+	print('Test of Lesk Model')
+	tst = STSModels()
+	tst.fit(filename_test, ['lesk'], 'test', per)
+	Xtst_lesk = tst.X
+	##
+	print('Prediction of Lesk Model')
+	pred = regr.predict(tst.X)
+
+	result = pearsonr(pred, tst.y)[0] # taking just the Pearson coeficient
+	print(result)
+	##
+
+	##
+	#############################################################
+	print()
+	print('#####################################')
+	print()
+	# Synsets
+	print('Train Synsets')
+	trn = STSModels()
+	trn.fit(filenames_train, ['synsets'], 'train', per)
+	##
+	X_syn = trn.X
+	regr = AdaBoostRegressor().fit(trn.X, trn.y)
+	##
+	print('Test of Synsets Model')
+	tst = STSModels()
+	tst.fit(filename_test, ['synsets'], 'test', per)
+	Xtst_syn = tst.X
+	##
+	print('Prediction of Synsets')
+	pred = regr.predict(tst.X)
+
+	result = pearsonr(pred, tst.y)[0] # taking just the Pearson coeficient
+	print(result)
+
+	#############################################################
+	print()
+	print('#####################################')
+	print()
+	## Lemmas + Lesk
+	print('Lemmas + Lesk + Synsets')
+	X = np.concatenate([X_lem, X_lesk, X_syn], axis=1)
+	regr = AdaBoostRegressor().fit(X, trn.y)
+	print('Prediction of Lemmas + Lesk + Synsets')
+	X_tst = np.concatenate([Xtst_lesk, Xtst_lemma, Xtst_syn], axis=1)
+	pred = regr.predict(X_tst)
+
+	result = pearsonr(pred, tst.y)[0] # taking just the Pearson coeficient
+	print(result)
+	#print(tst.y - pred)
+	print()
+	print(X.shape, len(trn.y))
+	print("-------------------------------------------")
+	print()
