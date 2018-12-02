696b985c830755b945bb5ff03b43bf07fd3f3d0a
jforjohn <giannisotiro@gmail.com>
2018-12-02 13:54:06 +0100

    STS:WIP: new baseline - add feature models

--- STSMain.py
+++ STSMain.py
@@ -1,8 +1,13 @@
+##
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag
 from nltk.corpus import wordnet as wn
+from nltk.metrics import jaccard_distance
+from sklearn.linear_model import LinearRegression
+from scipy.stats import pearsonr
+
 
 import re
 import pandas as pd
@@ -14,6 +19,40 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 wnl = WordNetLemmatizer()
+filenames_train = ['MSRpar', 'MSRvid', 'SMTeuroparl']
+filenames_test = ['MSRpar', 'MSRvid', 'SMTeuroparl',
+                  'surprise.OnWN', 'surprise.SMTnews']
+
+
+file_prefix = lambda stage: stage + '/STS'
+
+""" more orthodox and robust implementation """
+def dice_coefficient(s1, s2, n=2):
+    """dice coefficient 2nt/na + nb."""
+    if isinstance((s1), list):
+        a = ' '.join(s1)
+    elif isinstance(s1, str):
+        a = s1
+    if isinstance((s2), list):
+        b = ' '.join(s2)
+    elif isinstance(s2, str):
+        b = s2
+    if not len(a) or not len(b): return 0.0
+    if len(a) == 1:  a = a + u'.'
+    if len(b) == 1:  b = b + u'.'
+
+    a_bigram_list = []
+    for i in range(len(a) - 1):
+        a_bigram_list.append(a[i:i + n])
+    b_bigram_list = []
+    for i in range(len(b) - 1):
+        b_bigram_list.append(b[i:i + n])
+
+    a_bigrams = set(a_bigram_list)
+    b_bigrams = set(b_bigram_list)
+    overlap = len(a_bigrams & b_bigrams)
+    dice_coeff = overlap * 2.0 / (len(a_bigrams) + len(b_bigrams))
+    return dice_coeff
 
 def getData(filenames, file_prefix, **removals):
     input_prefix = file_prefix + '.input.'
@@ -45,7 +84,10 @@ def sentClean(sents, **removals):
             mod_sent = re.sub(r'[^A-Z a-z]', ' ', sent)
         else:
             mod_sent = re.sub(r'[^\w]', ' ', sent)  # |\b\w\b -> to take out single chars
-        new_sent.append(re.sub(r'[ ]+', ' ', mod_sent.strip()))
+        clean_sent = re.sub(r'[ ]+', ' ', mod_sent.strip())
+        if len(clean_sent) == 0:
+            clean_sent = sent
+        new_sent.append(clean_sent)
     return new_sent
 
 def sentTokenize(sents, **removals):
@@ -71,7 +113,12 @@ def lemmas(sents):
 
 def ngrams(sents, n, word=True):
     new_sent = []
+    n_old = n
     for sent in sents:
+        n = n_old
+        if len(sent) < n:
+            n_old = n
+            n = len(sent)
         if word:
             grams_lst = [w for w in nltk.ngrams(sent, n)]
             new_sent.append(grams_lst)
@@ -110,7 +157,7 @@ def mylesk(context_sentence, ambiguous_word, pos=None, synsets=None):
                         # (self.dice_coefficient(context, ex), ss_hyper, ss)
                     )
     _, hyper, sense = max(max_sense)
-    return sense
+    return hyper.lemmas()[0].name()
 
 def myWSD(pt_pair, context):
     '''
@@ -136,12 +183,38 @@ def lesk(sents):
         new_sent.append([myWSD(pair,sent) for pair in pairs])
     return new_sent
 
-filenames_train = ['MSRpar', 'MSRvid', 'SMTeuroparl']
-filenames_test = ['MSRpar', 'MSRvid', 'SMTeuroparl',
-                  'surprise.OnWN', 'surprise.SMTnews']
+def modelFeatures(df_sents):
+    Xlem = df_sents.apply(lemmas)
+    Xlesk = Xlem.apply(lesk)
 
+    Mdice_lem = Xlem.apply(lambda x: dice_coefficient(x['sentence1'], x['sentence2']), axis=1)
 
-file_prefix = lambda stage: stage + '/STS'
+    Mjac_lem = Xlem.apply(lambda x: jaccard_distance(set(x['sentence1']), set(x['sentence2'])), axis=1)
+
+    #Mlesk = Xlesk.apply(lambda x: jaccard_distance(set(x['sentence1']), set(x['sentence2'])), axis=1)
+    Mlesk = Xlesk.apply(lambda x: dice_coefficient(x['sentence1'], x['sentence2']), axis=1)
+    removals = {
+        'stop_words': True,
+        'numbers': True
+    }
+    Xngram = Xlem.apply(lambda col: ngrams(col, 2))
+    Mngram2 = Xngram.apply(lambda x: jaccard_distance(set(x['sentence1']), set(x['sentence2'])), axis=1)
+
+    Xngram = Xlem.apply(lambda col: ngrams(col, 4))
+    Mngram4 = Xngram.apply(lambda x: jaccard_distance(set(x['sentence1']), set(x['sentence2'])), axis=1)
+    '''
+    df_XtrainSW = getData(filenames_train, file_prefix('train'), **removals)
+    Xlem = df_XtrainSW.apply(lemmas)
+    Xngram = Xlem.apply(lambda col: ngrams(col, 1))
+    Mngram1_sw = Xngram.apply(lambda x: jaccard_distance(set(x['sentence1']), set(x['sentence2'])), axis=1)
+    Xngram = Xlem.apply(lambda col: ngrams(col, 2))
+    Mngram2_sw = Xngram.apply(lambda x: jaccard_distance(set(x['sentence1']), set(x['sentence2'])), axis=1)
+    
+    df = pd.concat([Mdice_lem, Mjac_lem, Mlesk, Mngram2, Mngram4, Mngram1_sw, Mngram2_sw], axis=1)
+    '''
+    df = pd.concat([Mdice_lem, Mjac_lem, Mlesk, Mngram2, Mngram4], axis=1)
+    return df
+
 #input_prefix = lambda stage:  file_prefix(stage) + '.input.'
 #gs_prefix = lambda stage: file_prefix(stage) + '.gs.'
 
@@ -151,20 +224,29 @@ removals = {
             }
 # Tokenized raw train data
 df_Xtrain = getData(filenames_train, file_prefix('train'), **removals)
-df_Xtrain.sample(frac=1)
+#df_Xtrain.sample(frac=1)
+labels_trn = df_Xtrain.iloc[:, -1]
+df_Xtrain = df_Xtrain.drop(df_Xtrain.columns[len(df_Xtrain.columns) - 1], axis=1)
 
 # Tokenized raw test data
-#df_Xtest = getData(filenames_test, file_prefix('test'), **removals)
+df_Xtest = getData(filenames_test, file_prefix('test'), **removals)
+labels_tst = df_Xtest.iloc[:, -1]
+df_Xtest = df_Xtest.drop(df_Xtest.columns[len(df_Xtest.columns) - 1], axis=1)
 
+'''
 Xtrn, Xval, ytrn, yval = train_test_split(
                                         df_Xtrain[['sentence1', 'sentence2']],
                                         df_Xtrain['golden_standard'],
                                         test_size=0.4)
                                         #random_state=0)
-Xlem = Xtrn.apply(lemmas)
-print(Xlem.head())
-n = 4
+'''
+##
+#print(Xlem.head())
 #Xngram = Xlem.apply(lambda col: ngrams(col, n))
 
-Xlesk = Xlem.apply(lesk)
-print(Xlesk.head())
+transformed_train = modelFeatures(df_Xtrain)
+reg = LinearRegression().fit(transformed_train, labels_trn)
+
+transformed_test = modelFeatures(df_Xtest)
+pred = reg.predict(transformed_test)
+print(pearsonr(pred.T.tolist(), labels_tst.tolist())[0])
